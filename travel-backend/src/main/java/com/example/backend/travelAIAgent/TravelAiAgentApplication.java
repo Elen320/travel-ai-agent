7ddf90e388f22c143c292849a92761e6593c1908
Elen <elen65750@gmail.com>
2026-03-26 17:36:17 +0400

    Spring-Boot setup

---
+++ travel-backend/src/main/java/com/example/backend/travelAIAgent/TravelAiAgentApplication.java
@@ -0,0 +1,11 @@
+package com.example.backend.travelAIAgent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAiAgentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TravelAiAgentApplication.class, args);
+    }
+}
